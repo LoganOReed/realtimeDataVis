1507cea3bff289294ed6c9e0b3b21259d6c78979
orlogan <orlogan7@gmail.com>
2023-08-16 04:40:19 -0400

    Basic Visual Done

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ to give continuous data vis
 https://github.com/RomelTorres/alpha_vantage
 
 https://www.alphavantage.co/documentation/
+
+https://www.freecodecamp.org/news/how-to-create-auto-updating-data-visualizations-in-python-with-matplotlib-and-aws/
--- realtimedatavis/data.py
+++ realtimedatavis/data.py
@@ -3,6 +3,8 @@ from pprint import pprint
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
+import mplfinance as mpf
+import datetime as datetime
 import seaborn as sns
 from alpha_vantage.async_support.timeseries import TimeSeries
 
@@ -12,15 +14,14 @@ apiCall = True
 symbols = [
     "GOOGL",
     "META",
-    "ORCL",
     "MSFT",
-    "ADBE",
 ]
+
 
 async def getData(symbol):
     # ts = TimeSeries(key="R0AU0EWLEJEKDL6W", output_format="pandas")
     ts = TimeSeries(key="G1ODTS0PTE9Q0JJC", output_format="pandas")
-    data, _ = await ts.get_intraday(symbol,interval="1min")
+    data, _ = await ts.get_intraday(symbol, interval="1min")
     await ts.close()
     return data
 
@@ -38,23 +39,49 @@ if apiCall:
         stockData[sym].to_csv(sym+"_temp.csv", index=True)
 
 else:
-    stockData ={"GOOGL": pd.read_csv("GOOGL_temp.csv")} 
+    stockData = {
+            "GOOGL": pd.read_csv("GOOGL_temp.csv",index_col=0,parse_dates=True),
+            "META": pd.read_csv("META_temp.csv",index_col=0,parse_dates=True),
+            "ORCL": pd.read_csv("ORCL_temp.csv",index_col=0,parse_dates=True),
+            "MSFT": pd.read_csv("MSFT_temp.csv",index_col=0,parse_dates=True),
+            "ADBE": pd.read_csv("ADBE_temp.csv",index_col=0,parse_dates=True)
+            }
+
 
 
 if __name__ == "__main__":
     pprint(stockData)
-    # plt.figure(figsize = (18,12))
-    # plt.boxplot(stockData["GOOGL"][["open","high","low","close"]].transpose())
+    print(stockData["GOOGL"][["time"]])
+    # sns.set_theme()
+    # plt.figure(figsize=(18, 12))
+    # plt.boxplot(stockData["GOOGL"][["open", "high", "low", "close"]].transpose())
     # plt.show()
-    sns.set_theme()
-    sns.catplot(data=stockData["GOOGL"],
-                x="time",
-                y=["open","high","low","close"],
-                # y="open",
-                hue="volume",
-                kind="box")
-    # sns.relplot(
-    #     data=stockData["GOOGL"],
-    #     x="date", y="open", col="close",
-    # )
+    # sns.catplot(data=stockData["GOOGL"][["date", "open", "high", "low", "close"]], kind="box")
+
+    # fig, axs = plt.subplots(ncols=4, nrows=2, figsize=(5.5,3.5), layout="constrained")
+    # # add an artist, in this case a nice label in the middle...
+    # for row in range(2):
+    #     for col in range(2):
+    #         axs[row, col].annotate(f'axs[{row}, {col}]', (0.5, 0.5),
+    #                                transform=axs[row, col].transAxes,
+    #                                ha='center', va='center', fontsize=18,
+    #                                color='darkgrey')
+    # axs[0, 0].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["open"])
+    # axs[0, 0].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["volume"])
+    # axs[0, 2].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["high"])
+    # axs[0, 3].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["low"])
+    # fig.suptitle('GOOGL')
+    # plt.show()
+    fig = mpf.figure(figsize=(9,6), style="starsandstripes")
+    ax1 = fig.add_subplot(2,3,1)
+    ax2 = fig.add_subplot(2,3,2)
+    ax3 = fig.add_subplot(2,3,3)
+    av1 = fig.add_subplot(3,3,7,sharex=ax1)
+    av2 = fig.add_subplot(3,3,8,sharex=ax1)
+    av3 = fig.add_subplot(3,3,9,sharex=ax3)
+
+    mpf.plot(stockData["GOOGL"].sort_index(ascending=True), ax=ax1, volume=av1, type="candle", mav=(3,9,15), xrotation=15, axtitle="GOOGL")
+    mpf.plot(stockData["META"].sort_index(ascending=True), ax=ax2, volume=av2, type="candle", mav=(3,9,15), xrotation=15, axtitle="META")
+    mpf.plot(stockData["MSFT"].sort_index(ascending=True), ax=ax3, volume=av3, type="candle", mav=(3,9,15), xrotation=15, axtitle="MSFT")
     plt.show()
+
