e9eab4be72daaf17018a8dc6ae5c699b1c9cf61d
orlogan <orlogan7@gmail.com>
2023-08-16 05:13:15 -0400

    Setup S3

--- realtimedatavis/data.py
+++ realtimedatavis/data.py
@@ -1,5 +1,5 @@
 import asyncio
-from pprint import pprint
+import boto3
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -42,36 +42,12 @@ else:
     stockData = {
             "GOOGL": pd.read_csv("GOOGL_temp.csv",index_col=0,parse_dates=True),
             "META": pd.read_csv("META_temp.csv",index_col=0,parse_dates=True),
-            "ORCL": pd.read_csv("ORCL_temp.csv",index_col=0,parse_dates=True),
             "MSFT": pd.read_csv("MSFT_temp.csv",index_col=0,parse_dates=True),
-            "ADBE": pd.read_csv("ADBE_temp.csv",index_col=0,parse_dates=True)
             }
 
 
 
 if __name__ == "__main__":
-    pprint(stockData)
-    print(stockData["GOOGL"][["time"]])
-    # sns.set_theme()
-    # plt.figure(figsize=(18, 12))
-    # plt.boxplot(stockData["GOOGL"][["open", "high", "low", "close"]].transpose())
-    # plt.show()
-    # sns.catplot(data=stockData["GOOGL"][["date", "open", "high", "low", "close"]], kind="box")
-
-    # fig, axs = plt.subplots(ncols=4, nrows=2, figsize=(5.5,3.5), layout="constrained")
-    # # add an artist, in this case a nice label in the middle...
-    # for row in range(2):
-    #     for col in range(2):
-    #         axs[row, col].annotate(f'axs[{row}, {col}]', (0.5, 0.5),
-    #                                transform=axs[row, col].transAxes,
-    #                                ha='center', va='center', fontsize=18,
-    #                                color='darkgrey')
-    # axs[0, 0].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["open"])
-    # axs[0, 0].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["volume"])
-    # axs[0, 2].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["high"])
-    # axs[0, 3].plot(pd.to_datetime(stockData["GOOGL"]["time"]), stockData["GOOGL"]["low"])
-    # fig.suptitle('GOOGL')
-    # plt.show()
     fig = mpf.figure(figsize=(9,6), style="starsandstripes")
     ax1 = fig.add_subplot(2,3,1)
     ax2 = fig.add_subplot(2,3,2)
@@ -83,5 +59,7 @@ if __name__ == "__main__":
     mpf.plot(stockData["GOOGL"].sort_index(ascending=True), ax=ax1, volume=av1, type="candle", mav=(3,9,15), xrotation=15, axtitle="GOOGL")
     mpf.plot(stockData["META"].sort_index(ascending=True), ax=ax2, volume=av2, type="candle", mav=(3,9,15), xrotation=15, axtitle="META")
     mpf.plot(stockData["MSFT"].sort_index(ascending=True), ax=ax3, volume=av3, type="candle", mav=(3,9,15), xrotation=15, axtitle="MSFT")
-    plt.show()
-
+    plt.savefig("stock_data.png")
+    # Save to AWS
+    s3 = boto3.resource('s3')
+    s3.meta.client.upload_file('stock_data.png', 'realtime-data-vis', 'stock_data.png', ExtraArgs={'ACL':'public-read'})
